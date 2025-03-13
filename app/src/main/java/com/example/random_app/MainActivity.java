91194f5761559b80e322a2eb3fc88a7aac8414dd
OWNER <wjddmarla081@naver.com>
2025-03-13 16:39:45 +0900

    commit test (1차 테스트)

---
+++ app/src/main/java/com/example/random_app/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.random_app;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textViewRandomNumber;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        textViewRandomNumber = findViewById(R.id.textViewRandomNumber);
+    }
+
+    public void generateRandomNumber(View view) {
+        Random random = new Random();
+        int randomnumber = random.nextInt(100);
+        textViewRandomNumber.setText("난수: " + randomnumber);
+
+
+    }
+}
